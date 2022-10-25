99dbac1352179794dc7ab3789382fe94ce340e6e
czkirilo <caiquez.kirilo@hotmail.com>
2022-10-25 19:55:30 -0300

    até o intervalo

---
+++ ChapterBET6/ChapterBET6/ChapterBET6/Controllers/UsuarioController.cs
@@ -0,0 +1,55 @@
+﻿using ChapterBET6.Interfaces;
+using ChapterBET6.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChapterBET6.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly IUsuarioRepository _iusuarioRepository;
+
+        public UsuarioController(IUsuarioRepository iusuarioRepository)
+        {
+            _iusuarioRepository = iusuarioRepository;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult Listar()
+        {
+            try
+            {
+                return Ok(_iusuarioRepository.Listar());
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+
+        [HttpGet("{id}")]
+        public IActionResult BuscarPorId(int id)
+        {
+            try
+            {
+                Usuario usuarioEncontrado = _iusuarioRepository.BuscarPorId(id);
+
+                if(usuarioEncontrado == null)
+                    return NotFound();
+
+                return Ok(usuarioEncontrado);
+            }catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
